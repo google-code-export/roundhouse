f5702edc35c5aab548cbb78c23d3d05b55c06a8b
ferventcoder <ferventcoder@7c4655ae-4ec9-11de-813f-f783bbce749d>
2009-07-25 12:22:28 +0000

    Upgraded UppercuT to version 122.

--- zip.bat
+++ zip.bat
@@ -7,5 +7,16 @@ SET DIR=%~d0%~p0%
 
 call "%DIR%build.bat"
 
+if %ERRORLEVEL% NEQ 0 goto errors
+
 SET build.config.settings="%DIR%Settings\UppercuT.config"
 "%DIR%lib\tools\Nant\nant.exe" %1 /f:.\BuildScripts\_zip.build -D:build.config.settings=%build.config.settings%
+
+if %ERRORLEVEL% NEQ 0 goto errors
+
+goto finish
+
+:errors
+EXIT /B %ERRORLEVEL%
+
+:finish
