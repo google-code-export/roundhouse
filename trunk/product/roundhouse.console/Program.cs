e659cabc7de387906b0438d32f7a0e269fb35ddf
ferventcoder <ferventcoder@7c4655ae-4ec9-11de-813f-f783bbce749d>
2009-09-29 22:12:53 +0000

    adding console application bones

---
+++ trunk/product/roundhouse.console/Program.cs
@@ -0,0 +1,17 @@
+﻿using log4net.Config;
+
+[assembly: XmlConfigurator(Watch = true)]
+
+namespace roundhouse.console
+{
+    using log4net;
+
+    internal class Program
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
