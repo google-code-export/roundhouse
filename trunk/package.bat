abd3bf5c0b868f128159ace163a7c1b07405e167
ferventcoder <ferventcoder@7c4655ae-4ec9-11de-813f-f783bbce749d>
2009-09-29 21:36:48 +0000

    upgrading to UC r188

--- trunk/package.bat
+++ trunk/package.bat
@@ -7,12 +7,3 @@ SET DIR=%~d0%~p0%
 
 SET build.config.settings="%DIR%Settings\UppercuT.config"
 "%DIR%lib\tools\Nant\nant.exe" %1 /f:.\BuildScripts\_package.build -D:build.config.settings=%build.config.settings%
-
-:: DEPRECATED
-:: call "%DIR%Settings\build.settings.bat"
-
-::"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\_package.build -D:dirs.db.project=%DB_PROJECT_FOLDER% -D:dirs.report.project=%REPORT_PROJECT_FOLDER%
-
-:: Use this option if you explicitly want to set the items in the _package.build
-
-::"%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\_package.build
