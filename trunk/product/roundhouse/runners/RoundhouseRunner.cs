970fb326df3b953c0794505dce7bdeb04b1ea1e9
ferventcoder <ferventcoder@7c4655ae-4ec9-11de-813f-f783bbce749d>
2009-10-10 07:30:51 +0000

    splitting out the migrator from the database

--- trunk/product/roundhouse/runners/RoundhouseRunner.cs
+++ trunk/product/roundhouse/runners/RoundhouseRunner.cs
@@ -3,13 +3,12 @@ namespace roundhouse.runners
     using System.IO;
     using infrastructure.filesystem;
     using infrastructure.logging;
-    using sql;
+    using migrators;
 
     public class RoundhouseRunner : IRunner
     {
-        private readonly string server_name;
-        private readonly string database_name;
-        private readonly string repository_url;
+        private readonly string repository_path;
+        private readonly string repository_version;
         private readonly string sql_files_directory;
         private readonly string up_folder_name;
         private readonly string down_folder_name;
@@ -18,14 +17,13 @@ namespace roundhouse.runners
         private readonly string views_folder_name;
         private readonly string sprocs_folder_name;
         private readonly string permissions_folder_name;
-        private readonly string version_table_name;
         private readonly FileSystemAccess file_system;
-        private readonly Database database;
+        private readonly DatabaseMigrator database_migrator;
         private const string SQL_EXTENSION = "*.sql";
 
-        public RoundhouseRunner(string server_name,
-                string database_name,
-                string repository_url,
+        public RoundhouseRunner(
+                string repository_path,
+                string repository_version,
                 string sql_files_directory,
                 string up_folder_name,
                 string down_folder_name,
@@ -34,13 +32,11 @@ namespace roundhouse.runners
                 string views_folder_name,
                 string sprocs_folder_name,
                 string permissions_folder_name,
-                string version_table_name,
                 FileSystemAccess file_system,
-                Database database)
+                DatabaseMigrator database_migrator)
         {
-            this.server_name = server_name;
-            this.database_name = database_name;
-            this.repository_url = repository_url;
+            this.repository_path = repository_path;
+            this.repository_version = repository_version;
             this.sql_files_directory = sql_files_directory;
             this.up_folder_name = up_folder_name;
             this.down_folder_name = down_folder_name;
@@ -49,21 +45,21 @@ namespace roundhouse.runners
             this.views_folder_name = views_folder_name;
             this.sprocs_folder_name = sprocs_folder_name;
             this.permissions_folder_name = permissions_folder_name;
-            this.version_table_name = version_table_name;
             this.file_system = file_system;
-            this.database = database;
+            this.database_migrator = database_migrator;
         }
 
         public void run()
         {
-            database.create_database(server_name, database_name);
+            database_migrator.create_database();
 
-            //todo: version the database first (can be backed out later)
+            // version the database first (can be backed out later)
+            database_migrator.version_the_database(repository_path, repository_version);
 
             traverse_files_and_run_sql(file_system.combine_paths(sql_files_directory, up_folder_name), true);
 
             //todo: remember when looking through all files below here, change CREATE to ALTER
-            //todo: we are going to create the create if not exists script
+            // we are going to create the create if not exists script
 
             traverse_files_and_run_sql(file_system.combine_paths(sql_files_directory, run_first_folder_name), true);
             traverse_files_and_run_sql(file_system.combine_paths(sql_files_directory, functions_folder_name), true);
@@ -85,7 +81,7 @@ namespace roundhouse.runners
             {
                 string sql_file_text = File.ReadAllText(sql_file);
                 Log.bound_to(this).log_an_info_event_containing("Found and running {0}.", sql_file);
-                database.run_sql(server_name, database_name, sql_file_text,file_system.get_file_name_from(sql_file),run_once);
+                database_migrator.run_sql(sql_file_text, file_system.get_file_name_from(sql_file), run_once);
             }
 
             foreach (string child_directory in file_system.get_all_directory_name_strings_in(directory))
@@ -93,6 +89,5 @@ namespace roundhouse.runners
                 traverse_files_and_run_sql(child_directory, run_once);
             }
         }
-
     }
 }
