c68895e16874978e03b113df1abcb1de7f4065b1
ferventcoder <ferventcoder@7c4655ae-4ec9-11de-813f-f783bbce749d>
2009-11-14 13:41:14 +0000

    Renamed RoundhouseRunner to RoundhouseMigrationRunner. Leaned out the tasks more by moving default property setup to ApplicationConfiguration. Started setting up items for the console to use most of the same ideas that the application uses.

--- trunk/product/roundhouse.console/Program.cs
+++ trunk/product/roundhouse.console/Program.cs
@@ -4,16 +4,64 @@
 
 namespace roundhouse.console
 {
+    using System;
+    using folders;
+    using infrastructure;
+    using infrastructure.containers;
+    using infrastructure.filesystem;
+    using infrastructure.logging;
     using log4net;
+    using migrators;
+    using resolvers;
+    using runners;
 
     internal class Program
     {
-        private static readonly ILog _logger = LogManager.GetLogger(typeof(Program));
+        private static readonly ILog _logger = LogManager.GetLogger(typeof (Program));
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            _logger.Info("HI!");
-            System.Console.Read();
+            ConfigurationPropertyHolder configuration = new ConsoleConfiguration(_logger);
+
+            //todo: throw back the help text if there isn't any
+            //todo: set configuration based on args
+
+            ApplicationConfiguraton.set_defaults_if_properties_are_not_set(configuration);
+
+
+            //todo: determine if this a call to the diff or the migrator
+            run_migrator(configuration);
+
+            if (!configuration.NonInteractive)
+            {
+                Console.Read();
+            }
+        }
+
+        public static void run_migrator(ConfigurationPropertyHolder configuration)
+        {
+            IRunner roundhouse_runner = new RoundhouseMigrationRunner(
+                configuration.RepositoryPath,
+                Container.get_an_instance_of<environments.Environment>(),
+                Container.get_an_instance_of<KnownFolders>(),
+                Container.get_an_instance_of<FileSystemAccess>(),
+                Container.get_an_instance_of<DatabaseMigrator>(),
+                Container.get_an_instance_of<VersionResolver>()
+                );
+            try
+            {
+                roundhouse_runner.run();
+            }
+            catch (Exception exception)
+            {
+                Log.bound_to(typeof (Program)).
+                    log_an_error_event_containing("{0} encountered an error:{1}{2}",
+                                                  ApplicationParameters.name, 
+                                                  Environment.NewLine, 
+                                                  exception
+                                                  );
+                throw;
+            }
         }
     }
 }
--- trunk/product/roundhouse/infrastructure/ApplicationConfiguraton.cs
+++ trunk/product/roundhouse/infrastructure/ApplicationConfiguraton.cs
@@ -16,14 +16,79 @@ namespace roundhouse.infrastructure
 
     public static class ApplicationConfiguraton
     {
-        public static void build_the_container(ConfigurationPropertyHolder property_holder)
+        public static void set_defaults_if_properties_are_not_set(ConfigurationPropertyHolder configuration_property_holder)
+        {
+            if (string.IsNullOrEmpty(configuration_property_holder.ServerName))
+            {
+                configuration_property_holder.ServerName = ApplicationParameters.default_server_name;
+            }
+
+            if (string.IsNullOrEmpty(configuration_property_holder.UpFolderName))
+            {
+                configuration_property_holder.UpFolderName = ApplicationParameters.default_up_folder_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.DownFolderName))
+            {
+                configuration_property_holder.DownFolderName = ApplicationParameters.default_down_folder_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.RunFirstFolderName))
+            {
+                configuration_property_holder.RunFirstFolderName = ApplicationParameters.default_run_first_folder_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.FunctionsFolderName))
+            {
+                configuration_property_holder.FunctionsFolderName = ApplicationParameters.default_functions_folder_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.ViewsFolderName))
+            {
+                configuration_property_holder.ViewsFolderName = ApplicationParameters.default_views_folder_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.SprocsFolderName))
+            {
+                configuration_property_holder.SprocsFolderName = ApplicationParameters.default_sprocs_folder_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.PermissionsFolderName))
+            {
+                configuration_property_holder.PermissionsFolderName = ApplicationParameters.default_permissions_folder_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.SchemaName))
+            {
+                configuration_property_holder.SchemaName = ApplicationParameters.default_roundhouse_schema_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.ScriptsRunTableName))
+            {
+                configuration_property_holder.ScriptsRunTableName = ApplicationParameters.default_scripts_run_table_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.VersionTableName))
+            {
+                configuration_property_holder.VersionTableName = ApplicationParameters.default_version_table_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.VersionFile))
+            {
+                configuration_property_holder.VersionFile = ApplicationParameters.default_version_file;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.VersionXPath))
+            {
+                configuration_property_holder.VersionXPath = ApplicationParameters.default_version_x_path;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.EnvironmentName))
+            {
+                configuration_property_holder.EnvironmentName = ApplicationParameters.default_environment_name;
+            }
+            if (string.IsNullOrEmpty(configuration_property_holder.OutputPath))
+            {
+                configuration_property_holder.OutputPath = ApplicationParameters.default_output_path;
+            }
+        }
+
+        public static void build_the_container(ConfigurationPropertyHolder configuration_property_holder)
         {
             IWindsorContainer windsor_container = new WindsorContainer();
 
             windsor_container.AddComponent<LogFactory, MultipleLoggerLogFactory>();
-            Logger nant_logger = new NAntLogger(property_holder.NAntTask);
-            Logger msbuild_logger = new MSBuildLogger(property_holder, property_holder.MSBuildTask.BuildEngine);
-            Logger log4net_logger = new Log4NetLogger(property_holder.Log4NetLogger);
+            Logger nant_logger = new NAntLogger(configuration_property_holder.NAntTask);
+            Logger msbuild_logger = new MSBuildLogger(configuration_property_holder, configuration_property_holder.MSBuildTask.BuildEngine);
+            Logger log4net_logger = new Log4NetLogger(configuration_property_holder.Log4NetLogger);
             Logger multi_logger = new MultipleLogger(new List<Logger> { nant_logger, msbuild_logger, log4net_logger });
             windsor_container.Kernel.AddComponentInstance<Logger>(multi_logger);
 
@@ -38,15 +103,15 @@ namespace roundhouse.infrastructure
 
             Database database_to_migrate = new SqlServerDatabase();
 
-            if (restore_from_file_ends_with_LiteSpeed_extension(property_holder.RestoreFromPath))
+            if (restore_from_file_ends_with_LiteSpeed_extension(configuration_property_holder.RestoreFromPath))
             {
                 database_to_migrate = new SqlServerLiteSpeedDatabase(database_to_migrate);
             }
-            database_to_migrate.server_name = property_holder.ServerName;
-            database_to_migrate.database_name = property_holder.DatabaseName;
-            database_to_migrate.roundhouse_schema_name = property_holder.SchemaName;
-            database_to_migrate.version_table_name = property_holder.VersionTableName;
-            database_to_migrate.scripts_run_table_name = property_holder.ScriptsRunTableName;
+            database_to_migrate.server_name = configuration_property_holder.ServerName;
+            database_to_migrate.database_name = configuration_property_holder.DatabaseName;
+            database_to_migrate.roundhouse_schema_name = configuration_property_holder.SchemaName;
+            database_to_migrate.version_table_name = configuration_property_holder.VersionTableName;
+            database_to_migrate.scripts_run_table_name = configuration_property_holder.ScriptsRunTableName;
             database_to_migrate.user_name = identity_of_runner;
 
             windsor_container.Kernel.AddComponentInstance<Database>(database_to_migrate);
@@ -54,27 +119,27 @@ namespace roundhouse.infrastructure
             CryptographicService crypto_provider = new MD5CryptographicService();
 
             DatabaseMigrator database_migrator = new DefaultDatabaseMigrator(windsor_container.Resolve<Database>(), crypto_provider,
-                                                                             property_holder.Restore, property_holder.RestoreFromPath, property_holder.OutputPath, !property_holder.WarnOnOneTimeScriptChanges);
+                                                                             configuration_property_holder.Restore, configuration_property_holder.RestoreFromPath, configuration_property_holder.OutputPath, !configuration_property_holder.WarnOnOneTimeScriptChanges);
             windsor_container.Kernel.AddComponentInstance<DatabaseMigrator>(database_migrator);
 
-            VersionResolver xml_version_finder = new XmlFileVersionResolver(windsor_container.Resolve<FileSystemAccess>(), property_holder.VersionXPath, property_holder.VersionFile);
-            VersionResolver dll_version_finder = new DllFileVersionResolver(windsor_container.Resolve<FileSystemAccess>(), property_holder.VersionFile);
+            VersionResolver xml_version_finder = new XmlFileVersionResolver(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.VersionXPath, configuration_property_holder.VersionFile);
+            VersionResolver dll_version_finder = new DllFileVersionResolver(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.VersionFile);
             IEnumerable<VersionResolver> resolvers = new List<VersionResolver> { xml_version_finder, dll_version_finder };
             VersionResolver version_finder = new ComplexVersionResolver(resolvers);
             windsor_container.Kernel.AddComponentInstance<VersionResolver>(version_finder);
 
-            Folder up_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), property_holder.SqlFilesDirectory, property_holder.UpFolderName, true);
-            Folder down_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), property_holder.SqlFilesDirectory, property_holder.DownFolderName, true);
-            Folder run_first_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), property_holder.SqlFilesDirectory, property_holder.RunFirstFolderName, false);
-            Folder functions_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), property_holder.SqlFilesDirectory, property_holder.FunctionsFolderName, false);
-            Folder views_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), property_holder.SqlFilesDirectory, property_holder.ViewsFolderName, false);
-            Folder sprocs_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), property_holder.SqlFilesDirectory, property_holder.SprocsFolderName, false);
-            Folder permissions_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), property_holder.SqlFilesDirectory, property_holder.PermissionsFolderName, false);
+            Folder up_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.SqlFilesDirectory, configuration_property_holder.UpFolderName, true);
+            Folder down_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.SqlFilesDirectory, configuration_property_holder.DownFolderName, true);
+            Folder run_first_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.SqlFilesDirectory, configuration_property_holder.RunFirstFolderName, false);
+            Folder functions_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.SqlFilesDirectory, configuration_property_holder.FunctionsFolderName, false);
+            Folder views_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.SqlFilesDirectory, configuration_property_holder.ViewsFolderName, false);
+            Folder sprocs_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.SqlFilesDirectory, configuration_property_holder.SprocsFolderName, false);
+            Folder permissions_folder = new DefaultFolder(windsor_container.Resolve<FileSystemAccess>(), configuration_property_holder.SqlFilesDirectory, configuration_property_holder.PermissionsFolderName, false);
 
             KnownFolders known_folders = new DefaultKnownFolders(up_folder, down_folder, run_first_folder, functions_folder, views_folder, sprocs_folder, permissions_folder);
             windsor_container.Kernel.AddComponentInstance<KnownFolders>(known_folders);
 
-            environments.Environment environment = new environments.DefaultEnvironment(property_holder.EnvironmentName);
+            environments.Environment environment = new environments.DefaultEnvironment(configuration_property_holder.EnvironmentName);
             windsor_container.Kernel.AddComponentInstance<environments.Environment>(environment);
 
 
