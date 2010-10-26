a1d66248447f00c0feee51f02b5555dd1ef7b52e
ferventcoder <ferventcoder@7c4655ae-4ec9-11de-813f-f783bbce749d>
2010-10-26 07:06:24 +0000

    remove after merge

--- branches/nhibernate/db/Oracle/TestRoundhousE/sprocs/usp_GetDate.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE PROCEDURE usp_GetDate 
-  (v_date OUT timestamp)
-AS
-BEGIN
-  SELECT ufn_GetDate('0') INTO v_date FROM dual;
-END;
--- branches/nhibernate/db/Oracle/TestRoundhousE/sprocs/usp_SelectTimmy.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE PROCEDURE usp_SelectTimmy 
- ( v_ID OUT Number )
-AS
-BEGIN
-SELECT ID INTO v_ID FROM vw_Dude;
-END;
