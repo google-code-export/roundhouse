e5476db50c8eec0c449514d14fb87fdc8eec5b69
ferventcoder <ferventcoder@7c4655ae-4ec9-11de-813f-f783bbce749d>
2012-01-24 23:03:15 +0000

    bottles, beaches... enabling package restore hard core...

---
+++ trunk/product/roundhouse.bottles.deployers/RoundhouseDeployer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Bottles;
+using Bottles.Deployment;
+using Bottles.Deployment.Runtime;
+using Bottles.Deployment.Runtime.Content;
+using Bottles.Diagnostics;
+using FubuCore;
+
+namespace roundhouse.bottles.deployers
+{
+    public class Roundhouse : IDirective
+    {
+        public string ConnectionString { get; set; }
+
+        public string Directory { get; set; }
+        public string GetDirectory()
+        {
+            return Directory ?? ".";
+        }
+    }
+
+    public class RoundhouseDeployer : IDeployer<Roundhouse>
+    {
+        private IBottleRepository _bottleRepository;
+
+        public RoundhouseDeployer(IBottleRepository bottleRepository)
+        {
+            _bottleRepository = bottleRepository;
+        }
+
+        public void Execute(Roundhouse directive, HostManifest host, IPackageLog log)
+        {
+            var destinationDirectory = directive.GetDirectory().AppendPath("roundhouse");
+
+            host.BottleReferences.Each(b =>
+            {
+                _bottleRepository.ExplodeFiles(new BottleExplosionRequest()
+                                               {
+                                                   BottleDirectory = BottleFiles.DataFolder,
+                                                   CopyBehavior =  CopyBehavior.overwrite,
+                                                   BottleName = b.Name,
+                                                   DestinationDirectory = destinationDirectory
+                                               });    
+            });
+
+            rh(directive, destinationDirectory);
+        }
+
+        void rh(Roundhouse directive, string destinationDirectory)
+        {
+            var migrate = new Migrate();
+            migrate.Set(cfg=>
+            {
+                cfg.ConnectionString = directive.ConnectionString;
+                cfg.SqlFilesDirectory = destinationDirectory;
+                cfg.Silent = true;
+            }).Run();
+        }
+
+        public string GetDescription(Roundhouse directive)
+        {
+            return "Running Roundhouse (rh.exe) ";
+        }
+
+    }
+
+}
